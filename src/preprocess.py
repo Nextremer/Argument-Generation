152cd21a122f101e683a57b05ef14c2aaa5e95d7
tomohikoabe <tomohiko.abe@nextremer.com>
2018-11-05 12:46:05 +0900

    小文字化の有無でmatch idxが異なるので処理をわけた

--- src/preprocess.py
+++ src/preprocess.py
@@ -31,44 +31,89 @@ def type_tagging(seq, match_idx, label, length):
 
 
 
+def get_match_idx1(match_idxs, essay_num, ann):
+    if len(match_idxs) == 1:
+        match_idx = match_idxs[0]
+
+    elif len(match_idxs) == 2:
+        if essay_num == 153:
+            match_idx = match_idxs[0]
+        elif essay_num == 163 or essay_num == 166 or essay_num == 181:
+            match_idx = match_idxs[1]
+        elif essay_num == 263:
+            if ann[0] == 'T2':
+                match_idx = match_idxs[0]
+            elif ann[0] == 'T14':
+                match_idx = match_idxs[1]
+        elif essay_num == 266:
+            if ann[0] == 'T1':
+                match_idx = match_idxs[0]
+            elif ann[0] == 'T10':
+                match_idx = match_idxs[1]
+        elif essay_num == 285:
+            if ann[0] == 'T4':
+                match_idx = match_idxs[1]
+            elif ann[0] == 'T14':
+                match_idx = match_idxs[0]
+                        
+    elif len(match_idxs) == 3:
+        if ann[0] == 'T1':
+            match_idx = match_idxs[1]
+        elif ann[0] == 'T3':
+            match_idx = match_idxs[2]
+
+    return match_idx
+
+
+
+def get_match_idx2(match_idxs, essay_num, ann):
+    if len(match_idxs) == 1:
+        match_idx = match_idxs[0]
+
+    elif len(match_idxs) == 2:
+        if essay_num == 153:
+            match_idx = match_idxs[0]
+        elif essay_num == 163 or essay_num == 166 or essay_num == 181:
+            match_idx = match_idxs[1]
+        elif essay_num == 263:
+            if ann[0] == 'T2':
+                match_idx = match_idxs[0]
+            elif ann[0] == 'T14':
+                match_idx = match_idxs[1]
+        elif essay_num == 285:
+            if ann[0] == 'T4':
+                match_idx = match_idxs[1]
+            elif ann[0] == 'T14':
+                match_idx = match_idxs[0]
+                        
+    elif len(match_idxs) == 3:
+        if ann[0] == 'T1':
+            match_idx = match_idxs[1]
+        elif ann[0] == 'T3':
+            match_idx = match_idxs[2]
+
+    return match_idx
+
+
+
 def get_labels(context, anns, essay_num, use_lower):
     """1つのcontextに対して、component typeのラベル系列を得る"""
     type_seq = [0]*len(context)
     for ann in anns:
         if ann[0].startswith('T'):
             pattern = ann[4:]
-            if args.use_lower:
+            if use_lower:
                 pattern = [w.lower() for w in pattern]
             pattern_length = len(pattern)
             idxs = [idx for idx, w in enumerate(context) if w == pattern[0]]
             match_idxs = list(filter(lambda x: context[x:x+pattern_length] == pattern, idxs))
-            
-            if len(match_idxs) == 1:
-                match_idx = match_idxs[0]
-            elif len(match_idxs) == 2:
-                if essay_num == 153:
-                    match_idx = match_idxs[np.argmin(match_idxs)]
-                elif essay_num == 163 or essay_num == 166 or essay_num == 181:
-                    match_idx = match_idxs[np.argmax(match_idxs)]
-                elif essay_num == 263:
-                    if ann[0] == 'T2':
-                        match_idx = match_idxs[np.argmin(match_idxs)]
-                    elif ann[0] == 'T14':
-                        match_idx = match_idxs[np.argmax(match_idxs)]
-                elif essay_num == 266:
-                    if ann[0] == 'T1':
-                        match_idx = match_idxs[np.argmin(match_idxs)]
-                elif essay_num == 285:
-                    if ann[0] == 'T4':
-                        match_idx = match_idxs[np.argmin(match_idxs)]
-                    elif ann[0] == 'T14':
-                        match_idx = match_idxs[np.argmax(match_idxs)]
-            elif len(match_idxs) == 3:
-                if ann[0] == 'T1':
-                    match_idx = match_idxs[1]
-                elif ann[0] == 'T3':
-                    match_idx = match_idxs[np.argmax(match_idxs)]
-        
+            assert len(match_idxs) > 0
+
+            if use_lower:
+                match_idx = get_match_idx1(match_idxs, essay_num, ann)
+            else:
+                match_idx = get_match_idx2(match_idxs, essay_num, ann)
+
         type_seq = type_tagging(type_seq, match_idx, ann[1], pattern_length)
 
     return type_seq
@@ -76,34 +121,36 @@ def get_labels(context, anns, essay_num, use_lower):
 
 def main(args):
     # tokenized topics, tokenized contexts, annotations
-    topics, contexts = read_txt(args.data_dir, args.use_lower)
+    topics, contexts = read_txt(args.data_dir)
     anns = read_ann(args.data_dir)
     assert len(topics) == len(contexts) == len(anns)
 
-    # save tokenized topics, tokenized contexts
-    with open(args.save_dir+'topics.pickle', 'wb') as f:
-        pickle.dump(topics, f)
-    with open(args.save_dir+'contexts.pickle', 'wb') as f:
-        pickle.dump(contexts, f)
+    if args.use_lower:
+        topics = [[w.lower() for w in topic] for topic in topics]
+        contexts = [[w.lower() for w in context] for context in contexts]
 
     type_seqs = []
     essay_num = 0
-    
     for context, ann in zip(contexts, anns):
         type_seq = get_labels(context, ann, essay_num, args.use_lower)
         type_seqs.append(type_seq)
         essay_num += 1
-        
-    # save type_seqs
-    with open(args.save_dir+'type_seqs.pickle', 'wb') as f:
-        pickle.dump(type_seqs, f)
+
+    # save tokenized topics, tokenized contexts, type sequences
+    if args.save_dir:
+        with open(args.save_dir+'topics.pickle', 'wb') as f:
+            pickle.dump(topics, f)
+        with open(args.save_dir+'contexts.pickle', 'wb') as f:
+            pickle.dump(contexts, f)
+        with open(args.save_dir+'type_seqs.pickle', 'wb') as f:
+            pickle.dump(type_seqs, f)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='preprocess corpus(Stab et al., 2017)')
     parser.add_argument('--data_dir', help='raw input data directory')
     parser.add_argument('--save_dir', help='save directory')
-    parser.add_argument('--use_lower', default=False)
+    parser.add_argument('--use_lower', action='store_true')
     args = parser.parse_args()
     
     main(args)
