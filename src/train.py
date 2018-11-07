08026be6196ca6c3c9c728f4aea3667c54535e14
tomohikoabe <tomohiko.abe@nextremer.com>
2018-11-07 17:14:50 +0900

    単語の埋め込みをgloveを使うように変更し、BIOラベルとtypeラベルを統一したラベルに変更し、BLEUスコアを計算するようにtrain.pyを変更。

--- src/train.py
+++ src/train.py
@@ -7,6 +7,8 @@ from collections import defaultdict
 from utils import *
 from model import *
 import time
+from nltk.translate import bleu_score
+
 
 
 def load_data(path):
@@ -27,6 +29,14 @@ def get_train_test_idxs(path):
                 s = line.strip().replace('"', '').split(';')
                 test_idxs.append(int(s[0].replace('essay', ''))-1)
     return train_idxs, test_idxs
+
+
+def calculateBleu(hypothesis, reference):
+    references = [reference]
+    list_of_references = [references]
+    list_of_hypotheses = [hypothesis]
+    bleu = bleu_score.corpus_bleu(list_of_references, list_of_hypotheses, smoothing_function=bleu_score.SmoothingFunction().method1)
+    return bleu
             
 
 def main(args):
@@ -45,7 +55,6 @@ def main(args):
     # load data(tokenized word) 
     topics = load_data(args.data_dir+'topics.pickle')
     contexts = load_data(args.data_dir+'contexts.pickle')
-    bio_seqs = load_data(args.data_dir+'bio_seqs.pickle')
     type_seqs = load_data(args.data_dir+'type_seqs.pickle')
     
     # train, test idxs
@@ -55,41 +64,40 @@ def main(args):
     train_size = len(train_idxs)
     test_size = len(test_idxs)
     
-    # initialize dictionary
-    source_w2id = defaultdict(lambda: len(source_w2id))
-    target_w2id = defaultdict(lambda: len(target_w2id))
+    # word2vec
+    f = open(args.w2vec_path, 'r')
+    w2vec = {line.strip().split(' ')[0]: np.array(line.strip().split(' ')[1:], dtype=np.float32) for line in f}
+    
+    # w2id
+    w2id = defaultdict(lambda: len(w2id))
     
     # train, test split
-    train_topics = [get_wid_seq(topics[idx], source_w2id, is_make=True) for idx in train_idxs]
-    train_contexts = [get_wid_seq(contexts[idx], target_w2id, is_make=True) for idx in train_idxs]
-    train_bio_seqs = [np.array(bio_seqs[idx], dtype=np.int32) for idx in train_idxs]
+    train_topics = [get_wid_seq(topics[idx], w2id, is_make=True) for idx in train_idxs]
+    train_contexts = [get_wid_seq(contexts[idx], w2id, is_make=True) for idx in train_idxs]
     train_type_seqs = [np.array(type_seqs[idx], dtype=np.int32) for idx in train_idxs]
     
     idxs = sorted(np.arange(0, train_size), key=lambda x: len(train_contexts[x]), reverse=True)
     train_topics = [train_topics[idx] for idx in idxs]
     train_contexts = [train_contexts[idx] for idx in idxs]
-    train_bio_seqs = [train_bio_seqs[idx] for idx in idxs]
     train_type_seqs = [train_type_seqs[idx] for idx in idxs]
     
-    test_topics = [get_wid_seq(topics[idx], source_w2id, is_make=False) for idx in test_idxs]
-    test_contexts = [get_wid_seq(contexts[idx], target_w2id, is_make=False) for idx in test_idxs]
-    test_bio_seqs = [np.array(bio_seqs[idx], dtype=np.int32) for idx in test_idxs]
+    test_topics = [topics[idx] for idx in test_idxs]
+    test_contexts = [contexts[idx] for idx in test_idxs]
     test_type_seqs = [np.array(type_seqs[idx], dtype=np.int32) for idx in test_idxs]
     
     idxs = sorted(np.arange(0, test_size), key=lambda x: len(test_contexts[x]), reverse=True)
     test_topics = [test_topics[idx] for idx in idxs]
     test_contexts = [test_contexts[idx] for idx in idxs]
-    test_bio_seqs = [test_bio_seqs[idx] for idx in idxs]
     test_type_seqs = [test_type_seqs[idx] for idx in idxs]
     
     # id2w
-    source_id2w = {v: k for k, v in source_w2id.items()}
-    target_id2w = {v: k for k, v in target_w2id.items()}
+    id2w = {v: k for k, v in w2id.items()}
     
     # define model
     model = Model(
-                source_w2id, 
-                target_w2id, 
+                w2id,
+                id2w,
+                w2vec,
                 args.n_layers, 
                 args.n_units, 
                 args.attn_n_units, 
@@ -110,10 +118,13 @@ def main(args):
     train_loss_w_reporter = ScoreReporter(args.mb_size, train_size)
     train_loss_label_reporter = ScoreReporter(args.mb_size, train_size)
     train_loss_reporter = ScoreReporter(args.mb_size, train_size)
+    train_bleu_reporter = ScoreReporter(args.mb_size, train_size)
     
     train_mean_losses_w = []
     train_mean_losses_label = []
     train_mean_losses = []
+    train_mean_bleus = []
+    test_mean_bleus = []
     
     # train test loop
     for epoch in range(args.max_epoch):
@@ -122,13 +133,12 @@ def main(args):
         for mb in range(0, train_size, args.mb_size):
             train_topic_mb = train_topics[mb:mb+args.mb_size]
             train_context_mb = train_contexts[mb:mb+args.mb_size]
-            train_bio_mb = train_bio_seqs[mb:mb+args.mb_size]
             train_type_mb = train_type_seqs[mb:mb+args.mb_size]
             
             model.cleargrads()
-            loss1, loss2, loss3, loss = model(train_topic_mb, train_context_mb, (train_bio_mb, train_type_mb))
+            loss1, loss2, loss = model(train_topic_mb, train_context_mb, train_type_mb)
             train_loss_w_reporter.add(backends.cuda.to_cpu(loss1.data))
-            train_loss_label_reporter.add(backends.cuda.to_cpu(args.eta*(loss2.data+loss3.data)))
+            train_loss_label_reporter.add(backends.cuda.to_cpu(args.eta*loss2.data))
             train_loss_reporter.add(backends.cuda.to_cpu(loss.data))
             loss.backward()
             optimizer.update()
@@ -136,66 +146,93 @@ def main(args):
         train_mean_losses_w.append(train_loss_w_reporter.mean())
         train_mean_losses_label.append(train_loss_label_reporter.mean())
         train_mean_losses.append(train_loss_reporter.mean())
-        print('train mean loss word: {}'.format(train_loss_w_reporter.mean()))
-        print('train mean loss eta*(loss2 + loss3): {}'.format(train_loss_label_reporter.mean()))
+        print('train mean loss(word): {}'.format(train_loss_w_reporter.mean()))
+        print('train mean loss(eta*loss2): {}'.format(train_loss_label_reporter.mean()))
         print('train mean loss: {}'.format(train_loss_reporter.mean()))
         
+        train_mean_bleu = 0
         # generate argument(train)
-        idxs = [0, 50, 100, 150, 200, 250, 300]
-        arguments = []
-        for idx in idxs:
-            print('-'*50)
+        for i in range(0, train_size, args.mb_size):
+            topic = ''
+            for w in topics[train_idxs[i]]:
+                topic += w
+                topic += ' '
+            train_idx_mb = train_idxs[i:i+args.mb_size]
+            train_topics_mb = [topics[j] for j in train_idx_mb]
+            arguments = model.generate(train_topics_mb, args.max_length)
+            
+            outs = []
+            for j, argument in enumerate(arguments):
+                bleu = calculateBleu(argument, contexts[train_idxs[i+j]])
+                train_mean_bleu += bleu
+                out = ''
+                for w in argument:
+                    out += w
+                    out += ' '  
+                outs.append(out)
+                
+            print('-'*100)
             print('train')
             print('topic:')
-            topic = ''
-            for i in train_topics[idx]:
-                topic += source_id2w[i]
-                topic += ' '
             print(topic)
             print('generated argument:')
-            argument = model.generate([train_topics[idx]], args.max_length)
-            print(argument)
-            arguments.append((topic, argument[0]))
+            print(outs[0])
+            
             if args.save_dir:
                 with open(args.save_dir+'arguments.txt', 'a') as f:
                     f.write('-'*50+'\n')
                     f.write('train\n')
                     f.write('epoch: '+str(epoch+1)+'\n')
                     f.write('topic: '+topic+'\n')
-                    f.write('argument: '+argument[0]+'\n')
+                    f.write('argument: '+outs[0]+'\n')
+            
+        train_mean_bleu /= float(train_size)
+        print('train mean bleu: '+str(train_mean_bleu))
+        train_mean_bleus.append(train_mean_bleu)
                     
-        print('-'*50)
-        
         # initialize train reporter
         train_loss_w_reporter = ScoreReporter(args.mb_size, train_size)
         train_loss_label_reporter = ScoreReporter(args.mb_size, train_size)
         train_loss_reporter = ScoreReporter(args.mb_size, train_size)
                 
+        
+        test_mean_bleu = 0
         # generate argument(test)
-        for idx in range(test_size):
-            print('-'*50)
-            print('test')
-            print('topic:')
+        for i in range(test_size):
             topic = ''
-            for i in test_topics[idx]:
-                topic += source_id2w[i]
+            for w in topics[test_idxs[i]]:
+                topic += w
                 topic += ' '
-            print(topic)
-            print('generated argument:')
-            argument = model.generate([test_topics[idx]], args.max_length)
-            print(argument)
+            argument = model.generate([topics[test_idxs[i]]], args.max_length)
+            out = ''
+            for w in argument[0]:
+                out += w
+                out += ' '
+            if i % 10 == 0:
+                print('-'*100)
+                print('test')
+                print('topic:')
+                print(topic)
+                print('generated argument:')
+                print(out)
             if args.save_dir:
                 with open(args.save_dir+'arguments.txt', 'a') as f:
                     f.write('-'*50+'\n')
                     f.write('test\n')
                     f.write('epoch: '+str(epoch+1)+'\n')
                     f.write('topic: '+topic+'\n')
-                    f.write('argument: '+argument[0]+'\n')
+                    f.write('argument: '+out+'\n')
+            bleu = calculateBleu(argument[0], contexts[test_idxs[i]])
+            test_mean_bleu += bleu
+        test_mean_bleu /= float(test_size)
+        test_mean_bleus.append(test_mean_bleu)
+        print('test mean bleu: '+str(test_mean_bleu))
                     
         if args.save_dir:
             # save figs
             save_figs(
-                args.save_dir, epoch+1, train_mean_losses, train_mean_losses_w, train_mean_losses_label)
+                args.save_dir, epoch+1, train_mean_losses, train_mean_losses_w, train_mean_losses_label, \
+                train_mean_bleus, test_mean_bleus)
             
         end_time = time.time()
         print('elapsed time:{}'.format(end_time-start_time))
@@ -207,6 +244,7 @@ if __name__ == '__main__':
     parser.add_argument('--data_dir', help='data directory')
     parser.add_argument('--idx_path', help='train test idx file')
     parser.add_argument('--save_dir', help='save figures directory')
+    parser.add_argument('--w2vec_path', help='w2vec path')
     parser.add_argument('--gpu', type=int, default=-1, help='GPU ID (negative value indicates CPU)')
     parser.add_argument('--n_layers', type=int, default=3)
     parser.add_argument('--n_units', type=int, default=200)
